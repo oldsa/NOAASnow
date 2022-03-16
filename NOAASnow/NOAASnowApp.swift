1458f555847861f669e4d96ffbc7ed0013b0b234
Anthony Olds <anthony.olds@gmail.com>
2022-03-16 13:30:43 -0600

    Initial Commit

---
+++ NOAASnow/NOAASnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOAASnowApp.swift
+//  NOAASnow
+//
+//  Created by Tony Olds on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct NOAASnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
